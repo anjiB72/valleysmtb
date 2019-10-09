ae37442687c33d15cdd5549116f1cc207f85e08a
anjiB72 <43617582+anjiB72@users.noreply.github.com>
2019-10-09 19:04:54 +0100

    added mobile menu and altered styling

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,12 @@
 //Hamburger menu icon to X change
+
 function myFunction(x) {
-    x.classList.toggle("change");
+      x.classList.toggle("change");
+      document.getElementById("mobile-menu").classList.toggle("display");
   }
+ 
+    //Hide menu when menu item clicked
+    $('.mob-menu a').on('click', function(){
+      $('.mob-menu').hide();
+    });
+
