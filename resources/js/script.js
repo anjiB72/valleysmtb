771bb628c7ee2a60d6ddc40a488b3e8df5ed65e5
anjiB72 <43617582+anjiB72@users.noreply.github.com>
2019-10-11 14:19:53 +0100

    added normal menu

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,9 +4,3 @@ function myFunction(x) {
       x.classList.toggle("change");
       document.getElementById("mobile-menu").classList.toggle("display");
   }
- 
-    //Hide menu when menu item clicked
-    $('.mob-menu a').on('click', function(){
-      $('.mob-menu').hide();
-    });
-
